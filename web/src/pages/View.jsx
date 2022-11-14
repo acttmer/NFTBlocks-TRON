1abd9354caea40a283338d95365be911d44cbd4e
Aego <aego.yu@gmail.com>
2022-11-14 15:11:36 -0800

    added deps

--- web/src/pages/View.jsx
+++ web/src/pages/View.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import TRC721ExtendableRoyaltyABI from '../abi/TRC721ExtendableRoyalty.json'
 import { getIPFSMetadataJSON, getIPFSURL } from '../common/ipfs'
 import { useTron } from '../contexts/Tron'
@@ -22,9 +22,12 @@ export default () => {
       const tokenURI = await instance.tokenURI(tokenId).call()
       const tokenMetadata = await getIPFSMetadataJSON(tokenURI)
       const dependencies = await instance.dependenciesOf(tokenId).call()
-      const royalties = await Promise.all(
-        dependencies.map(dep => instance.royaltiesOf(dep[1]).call()),
-      )
+      const royalties =
+        dependencies.length > 0
+          ? await Promise.all(
+              dependencies.map(dep => instance.royaltiesOf(dep[1]).call()),
+            )
+          : [await instance.royaltiesOf(tokenId).call()]
 
       console.log(royalties)
 
@@ -51,10 +54,22 @@ export default () => {
             src={getIPFSURL(data.tokenMetadata.image)}
           />
           <div>
+            <h3>Dependencies</h3>
+            <ul>
+              {data.dependencies.map((dep, index) => (
+                <li key={index}>
+                  <Link to={`/view/${dep[0]}/${dep[1].toNumber()}`}>
+                    {dep[0]} #{dep[1].toNumber()}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
             <h3>Royalties</h3>
             <ul>
-              {data.royalties.map(royalty => (
-                <li key={royalty[0][0]}>
+              {data.royalties.map((royalty, index) => (
+                <li key={index}>
                   {royalty[0][0]}: {royalty[0][1].toNumber() / 1e6} TRX
                 </li>
               ))}
