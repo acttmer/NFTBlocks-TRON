f2849a2ae9becf60e3d655971b09278a6ba4c14d
Aego <aego.yu@gmail.com>
2022-11-13 23:08:12 -0800

    added TRC721 and TRC165

---
+++ contracts/TRC165.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ITRC165.sol";
+
+abstract contract TRC165 is ITRC165 {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return interfaceId == type(ITRC165).interfaceId;
+    }
+}
